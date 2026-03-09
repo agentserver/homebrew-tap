ed2838fec52dc4486a1ae48c253d26ee5ccddba9
Mike Ehrmantraut <github@mryao.org>
2026-03-09 07:21:29 +0000

    chore: update agentserver formula to 0.18.2

--- Formula/agentserver.rb
+++ Formula/agentserver.rb
@@ -2,8 +2,8 @@
 class Agentserver < Formula
   desc "Connect local opencode to agentserver via WebSocket tunnel"
   homepage "https://github.com/agentserver/agentserver"
-  url "https://github.com/agentserver/agentserver/archive/refs/tags/v0.18.1.tar.gz"
-  sha256 "1a82acfe0705f60a35b4c4c9b3c037b3bedfc6ed1fc3f0307d6de824e9000094"
+  url "https://github.com/agentserver/agentserver/archive/refs/tags/v0.18.2.tar.gz"
+  sha256 "c09b3c1e8e5619ab4ae344bf8ca534fa3d5d1a93661deae20f48b1cb4d8dc219"
   license "MIT"
 
   depends_on "go" => :build
